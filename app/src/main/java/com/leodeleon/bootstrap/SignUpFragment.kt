7614bb2ce0c92b33932c04dee32577699b901deb
Leo Deleon <leodeleon.dev@outlook.com>
2019-09-30 14:50:22 +0200

    Motion layout

---
+++ app/src/main/java/com/leodeleon/bootstrap/SignUpFragment.kt
@@ -0,0 +1,22 @@
+package com.leodeleon.bootstrap
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+
+}
